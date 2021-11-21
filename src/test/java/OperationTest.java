2dbf2084331c494d5fef4a5cab395013ac8beb92
blessedboy14 <92112019+blessedboy14@users.noreply.github.com>
2021-11-21 14:24:03 +0300

    Create OperationTest.java

---
+++ src/test/java/OperationTest.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OperationTest {
+
+    @Test
+    @DisplayName("Этот тест проверяет функцию возвращения баланса")
+    void showBalance() {
+        String pin = "qwerty12345";
+        String cardNumber = "31597";
+        Operation operate = new Operation(cardNumber, pin);
+        int actual = operate.showBalance(cardNumber);
+        int expected = 68;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Этот тест проверяет функцию возвращения баланса")
+    void deposit() {
+        String pin = "Qdasd125fdaasd123eda%^32";
+        String cardNumber = "44356";
+        String memo = "hello";
+        int amount = 10;
+        Operation operate = new Operation(cardNumber, pin);
+        Assertions.assertTrue(operate.deposit(amount, cardNumber, memo));
+    }
+
+    @Test
+    @DisplayName("Этот тест проверяет функцию возвращения баланса")
+    void sendToOther() {
+        String pin = "123qwerty123";
+        String cardNumber = "18386";
+        String memo = "hello";
+        int amount = 10;
+        String cardNumber2 = "18386";
+        Operation operate = new Operation(cardNumber, pin);
+        Assertions.assertTrue(operate.sendToOther(amount,cardNumber2, cardNumber, memo));
+    }
+
+    @Test
+    @DisplayName("Этот тест проверяет функцию возвращения баланса")
+    void refill() {
+        String pin = "123451";
+        String cardNumber = "36897";
+        String memo = "hello";
+        int amount = 10;
+        Operation operate = new Operation(cardNumber, pin);
+        Assertions.assertTrue(operate.refill(amount, cardNumber, memo));
+    }
+
+    @Test
+    @DisplayName("Этот тест проверяет функцию возвращения баланса")
+    void showTransHistory() {
+        String pin = "sadaf";
+        String cardNumber = "98346";
+        Operation operate = new Operation(cardNumber, pin);
+        Assertions.assertTrue(operate.showTransHistory(cardNumber));
+    }
+}
