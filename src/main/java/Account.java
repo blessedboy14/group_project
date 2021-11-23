498c7b547c2263e2576a432cdb180a33afd1f33b
blessedboy14 <92112019+blessedboy14@users.noreply.github.com>
2021-11-23 18:54:03 +0300

    Update Account.java

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -2,17 +2,35 @@ import java.sql.*;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * @author Prokopchuk Daniil, Darya Samusenko, Alexandr Strukov
+ * @version 1.0
+ */
 public class Account {
-
+    /**
+     * This field is for first name of user
+     */
     private String firstName;
 
+    /**
+     * This field is for last name of user
+     */
     private String lastName;
 
+    /**
+     * It is a constructor for account class
+     * @param firstName first name
+     * @param lastName last name
+     */
     public Account(String firstName,String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
+    /**
+     * This method do all registration processes
+     * @return true if success, else false
+     */
     public Boolean register() {
 
         try(Connection c = DB.connection();Statement stmt = c.createStatement();
@@ -22,17 +40,18 @@ public class Account {
             stmt.executeUpdate(sql);
             String sql2 = "SELECT LAST_INSERT_ID()";
             ResultSet rs = stmt2.executeQuery(sql2);
-            int lastCountId = 0;
+            int last_count_id = 0;
             while (rs.next()) {
-                lastCountId = rs.getInt(1);
+                last_count_id = rs.getInt(1);
             }
             String cardNumber = this.generateCardNumber();
             String pin = this.inputPIN();
-            String sql3 = "INSERT INTO card VALUES('" + lastCountId + "'" +
+            String sql3 = "INSERT INTO card VALUES('" + last_count_id + "'" +
                     ", '" + cardNumber + "', '" +  pin + "')";
             stmt3.executeUpdate(sql3);
             String sql4 = "INSERT INTO balance VALUES('" + cardNumber + "', '0')";
             stmt4.executeUpdate(sql4);
+            c.close();
             System.out.println("\n\nNew Account was successfully created!\n");
             System.out.println("Card number :" + cardNumber);
             System.out.println("Pin : " + pin);
@@ -44,6 +63,10 @@ public class Account {
         }
     }
 
+    /**
+     * This method gets Pin from user
+     * @return pin for new user
+     */
     public String inputPIN() {
         String pin;
         Scanner sc = new Scanner(System.in);
@@ -52,6 +75,10 @@ public class Account {
         return pin;
     }
 
+    /**
+     * This method generates new unique card number
+     * @return unique card number with 5 symbols
+     */
     public String generateCardNumber() {
         String uuid;
         Random rng = new Random();
@@ -63,3 +90,4 @@ public class Account {
         return uuid;
     }
 }
+
