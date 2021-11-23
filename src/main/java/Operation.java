208ccd2015cb2853cf09d8cbfbac80cbff85b7fc
blessedboy14 <92112019+blessedboy14@users.noreply.github.com>
2021-11-23 18:54:44 +0300

    Update Operation.java

--- src/main/java/Operation.java
+++ src/main/java/Operation.java
@@ -1,18 +1,42 @@
 import java.sql.*;
 import java.util.Date;
 
+/**
+ * @author Prokopchuk Daniil, Darya Samusenko, Alexandr Strukov
+ * @version 1.0
+ */
 public class Operation {
 
+    /**
+     * This is card number for operations
+     */
     private String cardNumber;
 
+    /**
+     * This is pin code number for operations
+     */
     private String pinCode;
 
+    /**
+     * This is balance field for operations
+     */
     private int balance;
 
+    /**
+     * This is datetime field for operations
+     */
     private Date timestamp;
 
+    /**
+     * This is memo field for operations
+     */
     private String memo;
 
+    /**
+     * This is a constructor for Operation class
+     * @param cardNumber card number of user
+     * @param pinCode pin code of user
+     */
     public Operation(String cardNumber, String pinCode) {
         this.cardNumber = cardNumber;
         this.pinCode = pinCode;
@@ -20,9 +44,15 @@ public class Operation {
         this.memo = "";
     }
 
+    /**
+     * This method takes balance from DB
+     * @param cardNumber card number of user
+     * @return current user balance
+     */
     public int showBalance (String cardNumber) {
-        try(Connection c = DB.connection();
-            Statement stmt5 = c.createStatement()) {
+        try {
+            Connection c = DB.connection();
+            Statement stmt5 = c.createStatement();
             String sql5 = "SELECT * FROM balance WHERE cardNumber='"+ cardNumber + "'";
             ResultSet rs = stmt5.executeQuery(sql5);
             while (rs.next()) {
@@ -34,13 +64,21 @@ public class Operation {
         return this.balance;
     }
 
+    /**
+     * This method makes deposit to chosen card
+     * @param amount amount to deposit
+     * @param cardNumber card number of current user
+     * @param memo message with this transaction
+     * @return true if success and false if error
+     */
     public boolean deposit(int amount, String cardNumber, String memo){
-        try (Connection c = DB.connection();
+        try {
+            Connection c = DB.connection();
             Statement stmt6 = c.createStatement();
-            Statement stmt = c.createStatement()){
             String sql6 = "UPDATE balance SET balance= balance + '" + amount + "' WHERE cardNumber='" +
                     cardNumber + "'";
             stmt6.executeUpdate(sql6);
+            Statement stmt = c.createStatement();
             String sql = "INSERT INTO transactions VALUES(null,  NOW() , '" + memo + "', '"
             + amount + "', '" + cardNumber + "')";
             stmt.executeUpdate(sql);
@@ -51,18 +89,27 @@ public class Operation {
         }
     }
 
+    /**
+     * This method makes transfer to other card owner
+     * @param amount_other amount to send to other guy
+     * @param number_other card number of other user
+     * @param cardNumber current user card number
+     * @param memo message with this transaction
+     * @return  true if success and false if error
+     */
     public boolean sendToOther(int amount_other, String number_other, String cardNumber, String memo) {
-        try(Connection c = DB.connection();
+        try {
+            Connection c = DB.connection();
             Statement stmt7 = c.createStatement();
-            Statement stmt8 = c.createStatement();
-            Statement stmt = c.createStatement()) {
             String sql7 = "UPDATE balance SET balance = balance + '" + amount_other + "' WHERE cardNumber='" +
                     number_other + "'";
             stmt7.executeUpdate(sql7);
+            Statement stmt8 = c.createStatement();
             String sql8 = "UPDATE balance SET balance = balance - '" + amount_other + "' WHERE cardNumber='" +
                     cardNumber + "'";
             stmt8.executeUpdate(sql8);
             amount_other = -amount_other;
+            Statement stmt = c.createStatement();
             String sql = "INSERT INTO transactions VALUES(null,  NOW() , '" + memo + "', '"
                     + amount_other + "', '" + cardNumber + "')";
             stmt.executeUpdate(sql);
@@ -73,13 +120,21 @@ public class Operation {
         }
     }
 
-    public boolean refill(int amount, String cardNumber, String memo) {
-        try(Connection c = DB.connection();
+    /**
+     * This method helps user to withdrawal his money from card
+     * @param amount amount to withdrawal from your card
+     * @param cardNumber card number of current user
+     * @param memo message with this transaction
+     * @return true if success and false if error
+     */
+    public boolean withdrawal(int amount, String cardNumber, String memo) {
+        try{
+            Connection c = DB.connection();
             Statement stmt = c.createStatement();
-            Statement stmt2 = c.createStatement()){
             String sql = "UPDATE balance SET balance = balance - '" + amount + "' WHERE cardNumber='" +
                     cardNumber + "'";
             stmt.executeUpdate(sql);
+            Statement stmt2 = c.createStatement();
             amount = -amount;
             String sql2 = "INSERT INTO transactions VALUES(null,  NOW() , '" + memo + "', '"
                     + amount + "', '" + cardNumber + "')";
@@ -91,9 +146,15 @@ public class Operation {
         }
     }
 
+    /**
+     * This method returns user transactions history(only 5 last transactions)
+     * @param cardNumber card number of current user
+     * @return true if success and false if error
+     */
     public boolean showTransHistory(String cardNumber) {
-        try(Connection c = DB.connection();
-            Statement stmt = c.createStatement()){
+        try{
+            Connection c = DB.connection();
+            Statement stmt = c.createStatement();
             String sql = "SELECT * FROM transactions WHERE cardNumber='" + cardNumber + "' ORDER BY datetime DESC";
             ResultSet rs = stmt.executeQuery(sql);
             String result = "\n";
@@ -110,5 +171,4 @@ public class Operation {
             return false;
         }
     }
-
 }
